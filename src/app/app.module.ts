10722758e7917ca6bb601121c1edbdc40f69aa0e
Mikki <msmikkimonroe@gmail.com>
2021-02-22 10:47:23 -0600

    initial migrate app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,75 @@
+
+
+// import { SharedModule } from './shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage';
+
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+
 import { SafariViewController } from '@ionic-native/safari-view-controller/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { environment } from '../environments/environment';
+
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireFunctions, AngularFireFunctionsModule } from '@angular/fire/functions'; // USE_EMULATOR as USE_FUNCTIONS_EMULATOR
+
+// import { FirestoreService } from './services/firestore.service';
+
+import { Zoom } from '@ionic-native/zoom/ngx';
+
+// import { TranslateUniversalLoader } from './utils/translateuniversalloader';
 
 import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), IonicStorageModule.forRoot(), AppRoutingModule],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    AppRoutingModule,
+    IonicModule.forRoot(),
+    IonicStorageModule.forRoot(),
+    // TranslateModule.forRoot({
+    //   loader: {
+    //     provide: TranslateLoader,
+    //     useClass: TranslateUniversalLoader
+    //   }
+    // }),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireFunctionsModule,
+    AngularFirestoreModule,     // TODO AngularFirestoreModule.enablePersistence(),
+    AngularFireAuthModule,
+    BrowserAnimationsModule,
+  ],
   providers: [
     AuthService,
     SafariViewController,
+    InAppBrowser,
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    EmailComposer,
+    SocialSharing,
+    AngularFirestore,
+    TranslateService,
+    AngularFireAuth,
+    Zoom
   ],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,62 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  design: "ios",      // "md"
+  firebaseConfig: {
+    apiKey: "AIzaSyDTB6TRgJNOHvuq_L0YxNHSRufJx36y6dk",
+    authDomain: "anonymousmeetings.firebaseapp.com",
+    databaseURL: "https://anonymousmeetings.firebaseio.com",
+    projectId: "anonymousmeetings",
+    storageBucket: "anonymousmeetings.appspot.com",
+    messagingSenderId: "743896601158",
+    appId: "1:743896601158:web:460244580c9f6cdaaab047",
+    measurementId: "G-KJRD16EWZ5",
+    useEmulators: false
+  },
+  googleCloudConfig: {
+    agmKey: 'AIzaSyA4K1sIU-SOJkrOHTc3BzHurvTkg9N4Uf8'
+  },
+  logRocketConfig: {
+    appID: 'tdzfnj/anonymous-meetings',
+    options: {}
+  },
+  getUserRetry: 10,
+  getUserDelay: 500,
+  busyTimeoutDuration: 10000,
+  defaultSettings: {
+    darkTheme: true,
+    toastTimeout: 2000,
+    searchSettings: {
+      showHelp: true,
+      zipcode: null,    // zip -> gps
+      gps: true,
+      lat: null,
+      lon: null,
+      radius: 16,       // 16km (10 miles)
+  
+      byAnyDay: true,
+      byDay: 'Monday',      // today or dow
+      
+      // specify specific time
+      bySpecificTime: false,
+      bySpecific: {
+        start: null,
+        end: null,
+      },
+
+      // or
+      // (bySpecific & byRelative are mutually exclusive)
+  
+      // relative to current time
+      // use current time window range
+      byRelativeTime: false,
+      byRelative: { 
+        early: 2, 
+        late: 10 
+      }   
+    }
+  }
 };
 
 /*
